3c9af18642ed261b890e8493001590994721dc1c
Matt Ericksen <ericksenm93@ymail.com>
2020-07-22 23:39:13 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -13,7 +13,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {
+          :name =>
+          :age =>
+          :attitude =>
+        }
       ]
    }
 }
