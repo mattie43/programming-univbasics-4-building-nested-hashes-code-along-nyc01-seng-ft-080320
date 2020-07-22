810d44f1c129e9593810ad7de245311dfe9b0651
Matt Ericksen <ericksenm93@ymail.com>
2020-07-22 23:30:53 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -3,6 +3,9 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       :patriarch => {}
+      :patriarch => {}
+      :patriarch => {}
+      :patriarch => []
       }, 
     :capulet => {
       #your key/value pairs here!
